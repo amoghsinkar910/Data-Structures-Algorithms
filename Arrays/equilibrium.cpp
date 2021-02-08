1ce97e937f4edae7770e351690d55739193641b9
AMOGH <amogh@amogh.com>
2021-02-08 14:18:46 +0530

    Equilibrium point - sum from left and right is equal

---
+++ Arrays/equilibrium.cpp
@@ -0,0 +1,61 @@
+//author: _mayor
+#include <bits/stdc++.h>   
+
+#define for0(i, n) for (long long  i = 0; i < n; i++) 
+#define for1(i, n) for (long long  i = 1; i <= n; i++) 
+#define forc(i, l, r) for (long long  i = l; i <= r; ++i) 
+#define forr0(i, n) for (long long  i = n - 1; i >= 0; i--) 
+#define forr1(i, n) for (long long  i = n; i >= 1; i--) 
+
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end() 
+#define rall(x) (x).rbegin, (x).rend() 
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef long long int lli;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;  
+
+const ll N = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+int equilibriumPoint(long long a[], int n) 
+{
+    int i,j,totsum=0,l=0;
+    for(i=0;i<n;i++) totsum+=a[i];
+    
+    for(i=0;i<n;i++)
+    {
+        totsum-=a[i];
+        if(l==totsum) return i+1;
+        l+=a[i];
+    }
+    return -1;
+}
+
+int main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    { 
+        ll i,j,k,n,cnt=0;
+        cin>>n;
+        ll a[n];
+        for0(i,n) cin>>a[i];
+        cout << equilibriumPoint(a,n) <<"\n";
+    }
+    
+    return 0;
+}
