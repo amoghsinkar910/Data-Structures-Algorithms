e179d4d5c8622a25c559058ae27f3cc71ae76115
AMOGH <amoghsinkar910@gmail.com>
2021-02-23 11:43:18 +0530

    Add LCA in binary tree

---
+++ Trees/LCA-BT.cpp
@@ -0,0 +1,70 @@
+//author: _mayor
+#include <bits/stdc++.h>
+
+#define for0(i, n) for (long long i = 0; i < n; i++)
+#define for1(i, n) for (long long i = 1; i <= n; i++)
+#define forc(i, l, r) for (long long i = l; i <= r; ++i)
+#define forr0(i, n) for (long long i = n - 1; i >= 0; i--)
+#define forr1(i, n) for (long long i = n; i >= 1; i--)
+
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin, (x).rend()
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef long long int lli;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+const ll N = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+{
+    if (root == NULL)
+        return NULL;
+    if (root == p)
+        return p;
+    if (root == q)
+        return q;
+
+    TreeNode *left = lowestCommonAncestor(root->left, p, q);
+    TreeNode *right = lowestCommonAncestor(root->right, p, q);
+    if (left != NULL && right != NULL)
+        return root;
+
+    if (left)
+        return left;
+    return right;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    {
+        int i, j, k, n, cnt = 0;
+    }
+
+    return 0;
+}
