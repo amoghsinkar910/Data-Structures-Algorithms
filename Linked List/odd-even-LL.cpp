8c10a2b721a5337b16885d7dc0d8fa4b28a16106
AMOGH <amoghsinkar910@gmail.com>
2021-02-23 19:39:44 +0530

    Add odd even LL

---
+++ Linked List/odd-even-LL.cpp
@@ -0,0 +1,74 @@
+//author: _mayor
+#include <bits/stdc++.h>
+
+#define for0(i, n) for (long long i = 0; i < n; i++)
+#define for1(i, n) for (long long i = 1; i <= n; i++)
+#define forc(i, l, r) for (long long i = l; i <= r; ++i)
+#define forr0(i, n) for (long long i = n - 1; i >= 0; i--)
+#define forr1(i, n) for (long long i = n; i >= 1; i--)
+
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin, (x).rend()
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef long long int lli;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+const ll N = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *oddEvenList(ListNode *head)
+{
+    if (head == NULL)
+        return NULL;
+    ListNode *o = head, *e = head->next, *temp = head->next;
+
+    while (e != NULL && e->next != NULL)
+    {
+        o->next = e->next;
+        o = o->next;
+        e->next = o->next;
+        e = e->next;
+    }
+    o->next = temp;
+    return head;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll i, j, k, n, cnt = 0;
+        //init LL
+        //call oddEvenList()
+    }
+
+    return 0;
+}
