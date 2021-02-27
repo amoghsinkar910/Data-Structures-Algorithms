480fa7b6799a06d742d3ef8eca2ecce32dfc7594
AMOGH <amoghsinkar910@gmail.com>
2021-02-27 23:29:15 +0530

    Add rounds 693 and bye2020

---
+++ Codeforces/693/b.cpp
@@ -0,0 +1,60 @@
+//author: _mayor
+#include <bits/stdc++.h>   
+
+#define for0(i, n) for (long long  i = 0; i < n; i++) 
+#define for1(i, n) for (long long  i = 1; i <= n; i++) 
+#define forc(i, l, r) for (long long  i = l; i <= r; ++i) 
+#define forr0(i, n) for (long long  i = n - 1; i >= 0; i--) 
+#define forr1(i, n) for (long long  i = n; i >= 1; i--) 
+
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end() 
+#define rall(x) (x).rbegin, (x).rend() 
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef long long int lli;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;  
+
+const ll N = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+int main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    ll t;
+    cin>>t;
+    while(t--)
+    { 
+        ll i,n,one=0,two=0,s=0;
+        cin>>n;
+        for0(i,n)
+        {
+            ll x;
+            cin>>x;
+            if(x==1) one++;
+            else two++;
+            s+=x;
+        } 
+        if(s%2==0 && ((s/2)%2==0 || one>=1)) cout<<"YES"<<"\n";
+        else cout<<"NO"<<"\n";
+    }
+        
+    return 0;
+}
