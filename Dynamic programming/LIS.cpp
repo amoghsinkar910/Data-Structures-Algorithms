a1f2f44d9c2103ac32db3955df560d09d8ca1ecc
AMOGH <amoghsinkar910@gmail.com>
2021-03-01 23:23:16 +0530

    Add LIS

---
+++ Dynamic programming/LIS.cpp
@@ -0,0 +1,69 @@
+//author: _mayor
+#include <bits/stdc++.h>
+
+#define for0(i, n) for (long long i = 0; i < n; i++)
+#define for1(i, n) for (long long i = 1; i <= n; i++)
+#define forc(i, l, r) for (long long i = l; i <= r; ++i)
+#define forr0(i, n) for (long long i = n - 1; i >= 0; i--)
+#define forr1(i, n) for (long long i = n; i >= 1; i--)
+
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin, (x).rend()
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef long long int lli;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+const ll N = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+int lis(int a[], int n)
+{
+    int i, j;
+    int dp[n + 1];
+    dp[0] = 1;
+    for (i = 1; i < n; i++)
+    {
+        dp[i] = 1;
+        for (j = 0; j < i; j++)
+        {
+            if (a[i] > a[j] && dp[i] < 1 + dp[j])
+            {
+                dp[i] = 1 + dp[j];
+            }
+        }
+    }
+    return *max_element(dp, dp + n);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        int i, j, k, n, cnt = 0;
+        cin >> n;
+        int a[n];
+        for0(i, n) cin >> a[i];
+        cout << lis(a, n) << "\n";
+    }
+
+    return 0;
+}
