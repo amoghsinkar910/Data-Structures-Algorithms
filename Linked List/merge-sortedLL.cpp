ed69e4fca0913bb52c9678d54589f79fa0223a8d
AMOGH <amoghsinkar910@gmail.com>
2021-03-02 00:29:39 +0530

    Add merge two sorted LLs

---
+++ Linked List/merge-sortedLL.cpp
@@ -0,0 +1,82 @@
+//author: _mayor
+#include <bits/stdc++.h>
+
+#define for0(i, n) for (long long i = 0; i < n; i++)
+#define for1(i, n) for (long long i = 1; i <= n; i++)
+#define forc(i, l, r) for (long long i = l; i <= r; ++i)
+#define forr0(i, n) for (long long i = n - 1; i >= 0; i--)
+#define forr1(i, n) for (long long i = n; i >= 1; i--)
+
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin, (x).rend()
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef long long int lli;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+const ll N = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+{
+    ListNode *p = new ListNode();
+    ListNode *head = p;
+    while (l1 != NULL && l2 != NULL)
+    {
+        if (l1->val <= l2->val)
+        {
+            p->next = l1;
+            l1 = l1->next;
+        }
+        else
+        {
+            p->next = l2;
+            l2 = l2->next;
+        }
+        p = p->next;
+    }
+    if (l1 != NULL)
+    {
+        p->next = l1;
+    }
+    if (l2 != NULL)
+    {
+        p->next = l2;
+    }
+    return head->next;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    {
+        ll i, j, k, n, cnt = 0;
+         //call mergeTwoLists
+    }
+
+    return 0;
+}
