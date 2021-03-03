b7c715bd62a25c9293bf050a14237b11d1b1312c
AMOGH <amoghsinkar910@gmail.com>
2021-03-04 00:06:06 +0530

    Add search in 2D matrix

---
+++ Arrays/search-2d.cpp
@@ -0,0 +1,72 @@
+//author: _mayor
+#include <bits/stdc++.h>
+
+#define for0(i, n) for (long long i = 0; i < n; i++)
+#define for1(i, n) for (long long i = 1; i <= n; i++)
+#define forc(i, l, r) for (long long i = l; i <= r; ++i)
+#define forr0(i, n) for (long long i = n - 1; i >= 0; i--)
+#define forr1(i, n) for (long long i = n; i >= 1; i--)
+
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin, (x).rend()
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef long long int lli;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+const ll N = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+bool searchMatrix(vector<vector<int>> &a, int tar)
+{
+    int n = a.size();
+    int m = a[0].size();
+
+    int l = 0, r = n * m - 1;
+    while (l <= r)
+    {
+        int mid = (l + r) / 2;
+        int i = mid / m;
+        int j = mid % m;
+        if (a[i][j] == tar)
+            return true;
+        if (a[i][j] < tar)
+            l = mid + 1;
+        else
+            r = mid - 1;
+    }
+    return false;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll i, j, k, n, cnt = 0;
+        //...
+    }
+
+    return 0;
+}
