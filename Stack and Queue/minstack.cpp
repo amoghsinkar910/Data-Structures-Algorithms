d6157a7d7f1d73a9c8d21717a27768956db307d2
AMOGH <amoghsinkar910@gmail.com>
2021-03-03 10:55:16 +0530

    Add minstack

--- Stack and Queue/minstack.cpp
+++ Stack and Queue/minstack.cpp
@@ -1,18 +1,18 @@
 //author: _mayor
-#include <bits/stdc++.h>   
+#include <bits/stdc++.h>
 
-#define for0(i, n) for (long long  i = 0; i < n; i++) 
-#define for1(i, n) for (long long  i = 1; i <= n; i++) 
-#define forc(i, l, r) for (long long  i = l; i <= r; ++i) 
-#define forr0(i, n) for (long long  i = n - 1; i >= 0; i--) 
-#define forr1(i, n) for (long long  i = n; i >= 1; i--) 
+#define for0(i, n) for (long long i = 0; i < n; i++)
+#define for1(i, n) for (long long i = 1; i <= n; i++)
+#define forc(i, l, r) for (long long i = l; i <= r; ++i)
+#define forr0(i, n) for (long long i = n - 1; i >= 0; i--)
+#define forr1(i, n) for (long long i = n; i >= 1; i--)
 
 #define pb push_back
 #define fi first
 #define se second
 
-#define all(x) (x).begin(), (x).end() 
-#define rall(x) (x).rbegin, (x).rend() 
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin, (x).rend()
 
 using namespace std;
 
@@ -24,27 +24,51 @@ typedef long long ll;
 typedef long long int lli;
 typedef vector<ll> vll;
 typedef vector<vll> vvll;
-typedef double ld;  
+typedef double ld;
 
 const ll N = 1e5 + 5;
 const ll mod = 1e9 + 7;
 const ll INF = 1e9;
 
-int main() 
+stack<pair<int, int>> st;
+
+void push(int x)
+{
+    if (st.empty())
+    {
+        st.push({x, x});
+    }
+    else
+        st.push({x, min(x, st.top().second)});
+}
+
+void pop()
+{
+    if (!st.empty())
+        st.pop();
+}
+
+int top()
+{
+    return st.top().first;
+}
+
+int getMin()
+{
+    return st.top().second;
+}
+
+int main()
 {
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
+    cin.tie(0);
+    cout.tie(0);
     ll t;
-    cin>>t;
-    while(t--)
-    { 
-        ll i,j,k,n,cnt=0;
-           
+    cin >> t;
+    while (t--)
+    {
+        ll i, j, k, n, cnt = 0;
     }
-    
+
     return 0;
 }
