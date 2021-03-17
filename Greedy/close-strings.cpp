461225b4b967c70b16124272967fd1510dbf3bb4
AMOGH <amoghsinkar910@gmail.com>
2021-03-18 02:12:04 +0530

    Add close strings

---
+++ Greedy/close-strings.cpp
@@ -0,0 +1,77 @@
+//author: _mayor
+#include <bits/stdc++.h>
+
+#define for0(i, n) for (long long i = 0; i < n; i++)
+#define for1(i, n) for (long long i = 1; i <= n; i++)
+#define forc(i, l, r) for (long long i = l; i <= r; ++i)
+#define forr0(i, n) for (long long i = n - 1; i >= 0; i--)
+#define forr1(i, n) for (long long i = n; i >= 1; i--)
+
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin, (x).rend()
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef long long int lli;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+const ll N = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+bool closeStrings(string a, string b)
+{
+    int n = a.size(), m = b.size(), i;
+    if (m != n)
+        return false;
+
+    vector<int> c(26, 0);
+    vector<int> d(26, 0);
+    vector<int> e(26, 0);
+    vector<int> f(26, 0);
+
+    for (i = 0; i < n; i++)
+    {
+        c[a[i] - 'a']++;
+        d[a[i] - 'a'] = 1;
+    }
+
+    for (i = 0; i < m; i++)
+    {
+        e[b[i] - 'a']++;
+        f[b[i] - 'a'] = 1;
+    }
+
+    sort(c.begin(), c.end());
+    sort(e.begin(), e.end());
+    if (c == e && d == f)
+        return true;
+    return false;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    {
+        int i, j, k, n, cnt = 0;
+        string a, b;
+        cin >> a >> b;
+        cout << closeStrings(a, b) << "\n";
+    }
+
+    return 0;
+}
