5872fc18c7ad886d73f6abd79243fc08cdb92bc6
AMOGH <amoghsinkar910@gmail.com>
2021-04-02 01:49:11 +0530

    Add top-down approach for cses edit distance

--- Dynamic programming/cses/edit-dist.cpp
+++ Dynamic programming/cses/edit-dist.cpp
@@ -30,6 +30,33 @@ const ll N = 1e5 + 5;
 const ll mod = 1e9 + 7;
 const ll INF = 1e9;
 
+ll a[5005][5005];
+
+//top-down
+ll rec(string s1, string s2, ll i, ll j)
+{
+    ll ans;
+    if (i == s1.size())
+        return int(s2.size()) - j;
+    if (j == s2.size())
+        return int(s1.size()) - i;
+    if (i == s1.size() && j == s2.size())
+        return 0;
+
+    if (a[i][j] != -1)
+        return a[i][j];
+    if (s1[i] == s2[j])
+        return rec(s1, s2, i + 1, j + 1);
+    else
+    {
+        ll x = rec(s1, s2, i + 1, j);
+        ll y = rec(s1, s2, i, j + 1);
+        ll z = rec(s1, s2, i + 1, j + 1);
+        ans = 1 + min(x, min(y, z));
+    }
+    return a[i][j] = ans;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -45,7 +72,9 @@ int main()
         cin >> s1 >> s2;
         n = s1.size(), m = s2.size();
         ll dp[n + 1][m + 1];
+        memset(a, -1, sizeof(a));
 
+        //bottom-up
         for (i = 0; i <= n; i++)
         {
             for (j = 0; j <= m; j++)
@@ -64,6 +93,7 @@ int main()
         }
 
         cout << dp[n][m] << "\n";
+        //cout << rec(s1, s2, 0, 0) << "\n";
     }
 
     return 0;
