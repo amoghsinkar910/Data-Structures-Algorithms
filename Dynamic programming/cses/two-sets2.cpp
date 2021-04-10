60456d06b698f659c3863994936312ec4cba315e
AMOGH <amoghsinkar910@gmail.com>
2021-04-10 17:58:16 +0530

    Add cses two sets II

---
+++ Dynamic programming/cses/two-sets2.cpp
@@ -0,0 +1,71 @@
+//author: _mayor
+#include <bits/stdc++.h>
+
+#define for0(i, n) for (long long i = 0; i < n; i++)
+#define for1(i, n) for (long long i = 1; i <= n; i++)
+#define forc(i, l, r) for (long long i = l; i <= r; ++i)
+#define forr0(i, n) for (long long i = n - 1; i >= 0; i--)
+#define forr1(i, n) for (long long i = n; i >= 1; i--)
+
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin, (x).rend()
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef long long int lli;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+const ll N = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    {
+        ll i, j, n;
+        cin >> n;
+        ll sum = n * (n + 1) / 2;
+        if (sum % 2)
+            cout << 0 << "\n";
+        else
+        {
+            sum = sum / 2;
+            vector<vector<ll>> dp(n, vector<ll>(sum + 1, 0));
+            dp[0][0] = 1;
+            for (i = 1; i < n; i++)
+            {
+                for (j = 0; j <= sum; j++)
+                {
+                    dp[i][j] = dp[i - 1][j];
+                    ll rem = j - i;
+                    if (rem >= 0)
+                    {
+                        dp[i][j] += dp[i - 1][rem];
+                        dp[i][j] %= mod;
+                    }
+                }
+            }
+            cout << dp[n - 1][sum] << "\n";
+        }
+    }
+
+    return 0;
+}
