a32072e76e466ba180430f89e27ca333da828759
AMOGH <amoghsinkar910@gmail.com>
2021-04-17 17:40:03 +0530

    Add CF round 715

---
+++ Codeforces/715/c.cpp
@@ -0,0 +1,68 @@
+//author: _mayor
+#include <bits/stdc++.h>
+
+#define for0(i, n) for (long long i = 0; i < n; i++)
+#define for1(i, n) for (long long i = 1; i <= n; i++)
+#define forc(i, l, r) for (long long i = l; i <= r; ++i)
+#define forr0(i, n) for (long long i = n - 1; i >= 0; i--)
+#define forr1(i, n) for (long long i = n; i >= 1; i--)
+
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin, (x).rend()
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef long long int lli;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+const ll N = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+ll dp[2005][2005];
+ll a[2005];
+
+ll rec(ll l, ll r)
+{
+    if (l > r)
+        return 0;
+    if (dp[l][r] != -1)
+        return dp[l][r];
+    return dp[l][r] = a[r] - a[l] + min(rec(l + 1, r), rec(l, r - 1));
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    // ll t;
+    // cin>>t;
+    // while(t--)
+    {
+        ll i, j, k, n, cnt = 0;
+        cin >> n;
+        for0(i, n) cin >> a[i];
+
+        memset(dp, -1, sizeof(dp));
+        sort(a, a + n);
+        cout << rec(0, n - 1) << "\n";
+    }
+
+    return 0;
+}
