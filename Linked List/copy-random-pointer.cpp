67cb406283cacad810191e599d2e33d5034028ee
amoghsinkar910 <amoghsinkar910@gmail.com>
2021-05-26 20:03:28 +0530

    Add clone linked list with random pointer

---
+++ Linked List/copy-random-pointer.cpp
@@ -0,0 +1,91 @@
+//author: _mayor
+#include <bits/stdc++.h>
+
+#define for0(i, n) for (long long i = 0; i < n; i++)
+#define for1(i, n) for (long long i = 1; i <= n; i++)
+#define forc(i, l, r) for (long long i = l; i <= r; ++i)
+#define forr0(i, n) for (long long i = n - 1; i >= 0; i--)
+#define forr1(i, n) for (long long i = n; i >= 1; i--)
+
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin, (x).rend()
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef long long int lli;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+const ll N = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    Node *copyRandomList(Node *head)
+    {
+        unordered_map<Node *, Node *> mp;
+        Node *temp = head;
+        while (temp)
+        {
+            mp[temp] = new Node(temp->val);
+            temp = temp->next;
+        }
+
+        Node *copy;
+        temp = head;
+        while (temp)
+        {
+            copy = mp[temp];
+            copy->next = mp[temp->next];
+            copy->random = mp[temp->random];
+            temp = temp->next;
+        }
+
+        Node *root = mp[head];
+        return root;
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll i, j, k, n, cnt = 0;
+    }
+
+    return 0;
+}
