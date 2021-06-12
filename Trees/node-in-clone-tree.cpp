743f8f491521e89797d910e992716007744d52f4
amoghsinkar910 <amoghsinkar910@gmail.com>
2021-06-12 22:29:38 +0530

    Add search node in clone tree

---
+++ Trees/node-in-clone-tree.cpp
@@ -0,0 +1,63 @@
+//author: _mayor
+#include <bits/stdc++.h>
+
+#define for0(i, n) for (long long i = 0; i < n; i++)
+#define for1(i, n) for (long long i = 1; i <= n; i++)
+#define forc(i, l, r) for (long long i = l; i <= r; ++i)
+#define forr0(i, n) for (long long i = n - 1; i >= 0; i--)
+#define forr1(i, n) for (long long i = n; i >= 1; i--)
+
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin, (x).rend()
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef long long int lli;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+const ll N = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+TreeNode *ans;
+void rec(TreeNode *ori, TreeNode *clone, TreeNode *target)
+{
+    if (clone == NULL || ans)
+        return;
+    if (clone->val == target->val)
+    {
+        ans = clone;
+        return;
+    }
+    rec(ori->left, clone->left, target);
+    rec(ori->right, clone->right, target);
+}
+
+TreeNode *getTargetCopy(TreeNode *ori, TreeNode *clone, TreeNode *target)
+{
+    if (ori == NULL)
+        return NULL;
+    ans = NULL;
+    rec(ori, clone, target);
+    return ans;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    getTargetCopy()
+    return 0;
+}
