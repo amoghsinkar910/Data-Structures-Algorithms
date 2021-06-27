c778c218a080d2c07dfbcb71ab11f0bce57c95ec
amoghsinkar910 <amoghsinkar910@gmail.com>
2021-06-27 14:13:54 +0530

    Add rotate matrix in place

---
+++ Arrays/rotate-matrix.cpp
@@ -0,0 +1,69 @@
+//https://leetcode.com/problems/rotate-image/
+
+//author: _mayor
+#include <bits/stdc++.h>
+
+#define for0(i, n) for (long long i = 0; i < n; i++)
+#define for1(i, n) for (long long i = 1; i <= n; i++)
+#define forc(i, l, r) for (long long i = l; i <= r; ++i)
+#define forr0(i, n) for (long long i = n - 1; i >= 0; i--)
+#define forr1(i, n) for (long long i = n; i >= 1; i--)
+
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin, (x).rend()
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef long long int lli;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+const ll N = 1e5 + 5;
+const ll mod = 1e9 + 7;
+const ll INF = 1e9;
+
+void rotate(vector<vector<int>> &mat)
+{
+    //transpose(A) => reverse(A)
+    int i, j, k, r = mat.size(), c = mat[0].size();
+
+    //transpose
+    for (i = 0; i < r; i++)
+    {
+        for (j = 0; j < i; j++)
+        {
+            swap(mat[i][j], mat[j][i]);
+        }
+    }
+
+    //reverse
+    for (i = 0; i < r; i++)
+    {
+        for (j = 0; j < r / 2; j++)
+        {
+            swap(mat[i][j], mat[i][c - j - 1]);
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    {
+        ll i, j, k, n, cnt = 0;
+    }
+
+    return 0;
+}
